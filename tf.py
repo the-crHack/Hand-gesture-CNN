ef531bf8af906a983dbab638403542ce6ccb0bb4
the-crHack <68613549+the-crHack@users.noreply.github.com>
2022-08-15 13:36:16 +0530

    Add files via upload

---
+++ tf.py
@@ -0,0 +1,185 @@
+import tensorflow as tf
+import tflearn
+from tflearn.layers.conv import conv_2d,max_pool_2d
+from tflearn.layers.core import input_data,dropout,fully_connected
+from tflearn.layers.estimator import regression
+import numpy as np
+from PIL import Image
+import cv2
+import imutils
+
+# global variables
+bg = None
+
+def resizeImage(imageName):
+    basewidth = 100
+    img = Image.open(imageName)
+    wpercent = (basewidth/float(img.size[0]))
+    hsize = int((float(img.size[1])*float(wpercent)))
+    img = img.resize((basewidth,hsize), Image.ANTIALIAS)
+    img.save(imageName)
+
+def initialize_bg(image, aWeight):
+    global bg
+    # initialize the background
+    if bg is None:
+        bg = image.copy().astype("float")
+        return
+    cv2.accumulateWeighted(image, bg, aWeight)
+
+def partition(image, threshold=25):
+    global bg
+    # find the absolute difference between background and current frame
+    diff = cv2.absdiff(bg.astype("uint8"), image)
+
+    # threshold the diff image so that we get the foreground
+    thresholded = cv2.threshold(diff,
+                                threshold,
+                                255,
+                                cv2.THRESH_BINARY)[1]
+
+    # get the contours in the thresholded image
+    (cnts, _) = cv2.findContours(thresholded.copy(),
+                                    cv2.RETR_EXTERNAL,
+                                    cv2.CHAIN_APPROX_SIMPLE)
+
+    # return None, if no contours detected
+    if len(cnts) == 0:
+        return
+    else:
+        # based on contour area, get the maximum contour which is the hand
+        segmented = max(cnts, key=cv2.contourArea)
+        return (thresholded, segmented)
+
+def main():
+    # initialize weight for running average
+    aWeight = 0.5
+
+
+    camera = cv2.VideoCapture(0)
+    top, right, bottom, left = 10, 350, 225, 590
+
+    # initialize num of frames
+    num_frames = 0
+    start_recording = False
+
+    # keep looping, until interrupted
+    while(True):
+        (grabbed, frame) = camera.read()
+        frame = imutils.resize(frame, width = 700)
+
+        frame = cv2.flip(frame, 1)
+        clone = frame.copy()
+
+        (height, width) = frame.shape[:2]
+        roi = frame[top:bottom, right:left]
+
+        gray = cv2.cvtColor(roi, cv2.COLOR_BGR2GRAY)
+        gray = cv2.GaussianBlur(gray, (7, 7), 0)
+
+        if num_frames < 30:
+            initialize_bg(gray, aWeight)
+        else:
+            hand = partition(gray)
+
+            if hand is not None:
+                (thresholded, segmented) = hand
+
+
+                cv2.drawContours(clone, [segmented + (right, top)], -1, (0, 0, 255))
+                if start_recording:
+                    cv2.imwrite('1.png', thresholded)
+                    resizeImage('1.png')
+                    predictedClass, accuracy = getPredictedClass()
+                    showStatistics(predictedClass, accuracy)
+                cv2.imshow("Thresholded", thresholded)
+
+        # draw the segmented hand
+        cv2.rectangle(clone, (left, top), (right, bottom), (0,255,0), 2)
+
+        # increment the number of frames
+        num_frames += 1
+
+        # display the frame with segmented hand
+        cv2.imshow("Video Feed", clone)
+
+        # observe the keypress by the user
+        keypress = cv2.waitKey(1) & 0xFF
+
+        # if the user pressed "esc", then stop looping
+        if keypress == 27:
+            break
+        # if the user pressed "s", then start recording
+        if keypress == ord("s"):
+            start_recording = True
+
+def getPredictedClass():
+    # Predict
+    image = cv2.imread('1.png')
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    prediction = model.predict([gray_image.reshape(89, 100, 1)])
+    return np.argmax(prediction), (np.amax(prediction) / (prediction[0][0] + prediction[0][1] + prediction[0][2]))
+
+def showStatistics(predictedClass, accuracy):
+
+    textImage = np.zeros((300,512,3), np.uint8)
+    className = ""
+
+    if predictedClass == 0:
+        className = "a"
+    elif predictedClass == 1:
+        className = "d"
+    elif predictedClass == 2:
+        className = "f"
+
+    cv2.putText(textImage,"Predicted Class : " + className, 
+    (30, 30), 
+    cv2.FONT_HERSHEY_SIMPLEX, 
+    1,
+    (255, 255, 255),
+    2)
+
+    cv2.putText(textImage,"Accuracy : " + str(accuracy * 100) + '%', 
+    (30, 100), 
+    cv2.FONT_HERSHEY_SIMPLEX, 
+    1,
+    (255, 255, 255),
+    2)
+    cv2.imshow("Image_output", textImage)
+# Model defined
+#tf.reset_default_graph()
+tf.compat.v1.reset_default_graph()
+convnet=input_data(shape=[None,89,100,1],name='input')
+convnet=conv_2d(convnet,32,2,activation='relu')
+convnet=max_pool_2d(convnet,2)
+convnet=conv_2d(convnet,64,2,activation='relu')
+convnet=max_pool_2d(convnet,2)
+
+convnet=conv_2d(convnet,128,2,activation='relu')
+convnet=max_pool_2d(convnet,2)
+
+convnet=conv_2d(convnet,256,2,activation='relu')
+convnet=max_pool_2d(convnet,2)
+
+convnet=conv_2d(convnet,256,2,activation='relu')
+convnet=max_pool_2d(convnet,2)
+
+convnet=conv_2d(convnet,128,2,activation='relu')
+convnet=max_pool_2d(convnet,2)
+
+convnet=conv_2d(convnet,64,2,activation='relu')
+convnet=max_pool_2d(convnet,2)
+
+convnet=fully_connected(convnet,1000,activation='relu')
+convnet=dropout(convnet,0.75)
+
+convnet=fully_connected(convnet,3,activation='softmax')
+
+convnet=regression(convnet,optimizer='adam',learning_rate=0.001,loss='categorical_crossentropy',name='regression')
+
+model=tflearn.DNN(convnet,tensorboard_verbose=0)
+
+# Load Saved Model
+model.load("TrainedModel/GestureRecogModel2.tfl")
+
+main()
